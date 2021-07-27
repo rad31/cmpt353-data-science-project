bcf217bcaa5aa208c13223a5dcfd28d653c6baa8
Ronny Dorling <ronnydorling@gmail.com>
2021-07-27 08:51:20 -0700

    Update voter data extraction to convert to single Pandas DataFrame and resulting csv

--- src/extract-voter-data.py
+++ src/extract-voter-data.py
@@ -2,19 +2,62 @@ import sys
 import pandas as pd
 from pyspark.sql import SparkSession, functions, types, udf
 
+spark = SparkSession.builder.appName('data-clean').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+assert sys.version_info >= (3,5)
+assert spark.version >= '3.1'
+
 def percent_republican(party, n_votes, total_votes):
     if party == 'REPUBLICAN':
         return n_votes / total_votes
     return 0.0
 
-def main():
-    spark = SparkSession.builder.appName('data-clean').getOrCreate()
-    spark.sparkContext.setLogLevel('WARN')
-    assert sys.version_info >= (3,5)
-    assert spark.version >= '3.1'
+# Extract the percentage of Republican voters for each county
+def get_percent_republican(df):
+    percent_republican_udf = functions.udf(percent_republican, returnType=types.DoubleType())
+    
+    desired_election_data = df.select(
+        df['state'],
+        df['county_name'],
+        (percent_republican_udf(
+            df['party'],
+            df['candidatevotes'],
+            df['totalvotes'],
+        )).alias('percent_republican'),
+    )
 
+    return desired_election_data.filter(desired_election_data['percent_republican'] > 0.0)
 
-    raw_election_data = spark.read.csv('../data/countypres_2000-2020.csv', header=True)
+# Extract only the rows with the winning candidate for each county
+def get_winning_candidates(df):
+    desired_election_data1 = df.select(
+        df['state'],
+        df['county_name'],
+        df['party'],
+        df['candidatevotes'],
+        df['totalvotes'],
+    )
+
+    df.registerTempTable('data')
+    query = """
+        SELECT state, county_name, max(candidatevotes) as candidatevotes
+        FROM data
+        GROUP BY state, county_name
+    """
+
+    desired_election_data2 = spark.sql(query)
+
+    final_data = desired_election_data2.join(
+        desired_election_data1,
+        on=['state', 'county_name', 'candidatevotes'],
+        how='left'
+    )
+
+    return final_data
+
+def main(input_file):
+    raw_election_data = spark.read.csv(input_file, header=True)
 
     # Extract only election results from 2016, cast votes as int
     election_data_2016 = raw_election_data.filter(raw_election_data['year'] == 2016)
@@ -35,47 +78,15 @@ def main():
         valid_election_data_2016['totalvotes'].isNotNull()
     )
 
-    ### Get percentage of Republican voters ###
+    # Get percentage of Republican voters for each county
+    final_data = get_percent_republican(valid_election_data_2016)
 
-    percent_republican_udf = functions.udf(percent_republican, returnType=types.DoubleType())
-    
-    desired_election_data = valid_election_data_2016.select(
-        valid_election_data_2016['state'],
-        valid_election_data_2016['county_name'],
-        (percent_republican_udf(
-            valid_election_data_2016['party'],
-            valid_election_data_2016['candidatevotes'],
-            valid_election_data_2016['totalvotes'],
-        )).alias('percent_republican'),
-    )
+    # Alternatively, we could get the winning candidate for each county
+    # final_data = get_winning_candidates(valid_election_data_2016)
 
-    final_data = desired_election_data.filter(desired_election_data['percent_republican'] > 0.0)
-
-    ### Get winning candidate only ###
-
-    # desired_election_data1 = valid_election_data_2016.select(
-    #     valid_election_data_2016['state'],
-    #     valid_election_data_2016['county_name'],
-    #     valid_election_data_2016['party'],
-    #     valid_election_data_2016['candidatevotes'],
-    #     valid_election_data_2016['totalvotes'],
-    # )
-
-    # valid_election_data_2016.registerTempTable('data')
-    # query = """
-    #     SELECT state, county_name, max(candidatevotes) as candidatevotes
-    #     FROM data
-    #     GROUP BY state, county_name
-    # """
-    # desired_election_data2 = spark.sql(query)
-    # final_data = desired_election_data2.join(
-    #     desired_election_data1,
-    #     on=['state', 'county_name', 'candidatevotes'],
-    #     how='left'
-    # )
-
-    final_data.write.csv('../data/percent-republican-voters')
+    final_data.toPandas().to_csv('../data/extracted/percent-republican-voters.csv', header=True)
 
 
 if __name__ == '__main__':
-    main()
+    input_file = '../data/countypres_2000-2020.csv'
+    main(input_file)
